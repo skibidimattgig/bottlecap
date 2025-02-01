cdad4af8e2e9299c3583495756d161bf71cfcbee
skibidimattgig <zedroxtt@gmail.com>
2025-02-01 14:58:25 +0200

    Update bottlecaps.lua

--- bottlecaps.lua
+++ bottlecaps.lua
@@ -1,15 +1,8 @@
-local things = game
 local rs = game.ReplicatedStorage
-local n = rs.Network
-local e = n.TakeBottleCap
+local e = rs:FindFirstChild("TakeBottleCap")
 
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
-
-local char = player.Character
-local hrp = char.HumanoidRootPart
-
-local mouse = player:GetMouse()
 
 while player do 
     for _, v in workspace:GetChildren() do
