30d1b147aa545ebd107aad627482c8fd57b1868a
skibidimattgig <zedroxtt@gmail.com>
2025-02-01 12:55:19 +0200

    Create bottlecaps.lua

---
+++ bottlecaps.lua
@@ -0,0 +1,25 @@
+local things = game
+local rs = game.ReplicatedStorage
+local n = rs.Network
+local e = n.TakeBottleCap
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local char = player.Character
+local hrp = char.HumanoidRootPart
+
+local mouse = player:GetMouse()
+
+while player do 
+    for _, v in workspace:GetChildren() do
+        if not string.find(v.Name, "Pile") and not v.Name == "BottleCap" then continue end
+        if not v:FindFirstChild("BottleCap", true) then continue end
+
+        for _, b in v:GetDescendants() do
+            if b.Name ~= "BottleCap" then continue end
+            e:FireServer(b)
+        end
+    end
+task.wait(0.325)
+end
