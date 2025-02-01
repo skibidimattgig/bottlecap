0adbaa5a0d1a2e03080a95c656f77ee1a2679f39
skibidimattgig <zedroxtt@gmail.com>
2025-02-01 12:59:42 +0200

    Update eventbodypos.lua

--- eventbodypos.lua
+++ eventbodypos.lua
@@ -8,7 +8,7 @@ coroutine.wrap(function()
   
   local pos = hrp.CFrame
   
-  local e = game.ReplicatedStorage:FindFirstChild("OPEN_INVENTORY")
+  local e = game.ReplicatedStorage:FindFirstChild("OPEN_INVENTORY", true)
   
   local position1 = pos.Position -- Starting position
   local position2 = Vector3.new(-200, 100, -9100) -- Target position
