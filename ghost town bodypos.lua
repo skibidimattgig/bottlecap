e8df6334d91e9a53bf5733d3c72fb3ec16223d89
skibidimattgig <zedroxtt@gmail.com>
2025-02-01 18:08:03 +0200

    Update ghost town bodypos.lua

--- ghost town bodypos.lua
+++ ghost town bodypos.lua
@@ -39,7 +39,7 @@ coroutine.wrap(function()
 
   local position1 = pos.Position -- Starting position
 
-  local position2 = Vector3.new(-200, 150, -15000) -- Target position
+  local position2 = Vector3.new(-200, 150, -120000) -- Target position
   
   -- Time to complete the movement
   local moveDuration = 30 -- Seconds
