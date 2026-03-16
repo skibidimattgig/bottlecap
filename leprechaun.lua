e190884888609f5d22a6c935f57630f590b85c32
skibidimattgig <zedroxtt@gmail.com>
2026-03-16 16:56:26 +0200

    Update leprechaun.lua

--- leprechaun.lua
+++ leprechaun.lua
@@ -91,7 +91,7 @@ local function start()
                     waiting = true
 
                     hrp.Anchored = false
-                    hrp.CFrame = CFrame.new(Vector3.new(roadPos.X, roadpos.Y, roadpos.Z))
+                    hrp.CFrame = CFrame.new(Vector3.new(roadPos.X, roadPos.Y+4, roadPos.Z))
                     hrp.AssemblyLinearVelocity = Vector3.new(0,0,0)
                     task.delay(0.5, function()
                         hrp.Anchored = true
